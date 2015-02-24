c401efb75010617237106d4fdd3b68d939942c83
Alexander01998 <contact.wurstimperium@gmail.com>
2015-02-24 20:18:52 +0100

    Add calculation for getting the best armor

--- Wurst Client/src/tk/wurst_client/module/modules/AutoArmor.java
+++ Wurst Client/src/tk/wurst_client/module/modules/AutoArmor.java
@@ -7,11 +7,20 @@
  */
 package tk.wurst_client.module.modules;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import tk.wurst_client.module.Category;
 import tk.wurst_client.module.Module;
 
 public class AutoArmor extends Module
 {
+	private int bestHelmet;
+	private int bestPlate;
+	private int bestLegs;
+	private int bestBoots;
+
 	public AutoArmor()
 	{
 		super("AutoArmor",
@@ -23,6 +32,30 @@ public class AutoArmor extends Module
 	@Override
 	public void onUpdate()
 	{
-		
+		if(!getToggled())
+			return;
+		bestHelmet = 0;
+		bestPlate = 0;
+		bestLegs = 0;
+		bestBoots = 0;
+		for(int i = 9; i < 45; i++)
+		{
+			ItemStack itemstack = Minecraft.getMinecraft().thePlayer.inventory.getStackInSlot(i);
+			Item item = itemstack.getItem();
+			if(itemstack == null || item == null)
+				continue;
+			if(item instanceof ItemArmor)
+			{
+				ItemArmor armor = (ItemArmor)item;
+				if(armor.armorType == 0 && armor.damageReduceAmount > bestHelmet)
+					bestHelmet = i;
+				if(armor.armorType == 1 && armor.damageReduceAmount > bestPlate)
+					bestPlate = i;
+				if(armor.armorType == 2 && armor.damageReduceAmount > bestLegs)
+					bestLegs = i;
+				if(armor.armorType == 3 && armor.damageReduceAmount > bestBoots)
+					bestBoots = i;
+			}
+		}
 	}
 }
