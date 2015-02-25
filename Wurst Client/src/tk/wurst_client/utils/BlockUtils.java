adff41be95c9510cc21decb67b321cc5a491a5c0
Alexander01998 <contact.wurstimperium@gmail.com>
2015-02-25 22:00:18 +0100

    Add BlockUtils methods that ignore height

--- Wurst Client/src/tk/wurst_client/utils/BlockUtils.java
+++ Wurst Client/src/tk/wurst_client/utils/BlockUtils.java
@@ -41,13 +41,21 @@ public class BlockUtils
 				Minecraft.getMinecraft().thePlayer.onGround
 			));
 	}
-	
-	public static float getBlockDistanceFromPlayer(BlockPos blockPos)
+
+	public static void faceBlockClientHorizontally(BlockPos blockPos)
 	{
-		return getBlockDistanceFromPlayer(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+		double diffX = blockPos.getX() + 0.5 - Minecraft.getMinecraft().thePlayer.posX;
+		double diffZ = blockPos.getZ() + 0.5 - Minecraft.getMinecraft().thePlayer.posZ;
+		float yaw = (float)(Math.atan2(diffZ, diffX) * 180.0D / Math.PI) - 90.0F;
+		Minecraft.getMinecraft().thePlayer.rotationYaw = Minecraft.getMinecraft().thePlayer.rotationYaw + MathHelper.wrapAngleTo180_float(yaw - Minecraft.getMinecraft().thePlayer.rotationYaw);
+	}
+	
+	public static float getPlayerBlockDistance(BlockPos blockPos)
+	{
+		return getPlayerBlockDistance(blockPos.getX(), blockPos.getY(), blockPos.getZ());
 	}
 
-	public static float getBlockDistanceFromPlayer(double posX, double posY, double posZ)
+	public static float getPlayerBlockDistance(double posX, double posY, double posZ)
 	{
 		float xDiff = (float)(Minecraft.getMinecraft().thePlayer.posX - posX);
 		float yDiff = (float)(Minecraft.getMinecraft().thePlayer.posY - posY);
@@ -58,5 +66,12 @@ public class BlockUtils
 	public static float getBlockDistance(float xDiff, float yDiff, float zDiff)
 	{
 		return MathHelper.sqrt_float((xDiff - 0.5F) * (xDiff - 0.5F) + (yDiff - 0.5F) * (yDiff - 0.5F) + (zDiff - 0.5F) * (zDiff - 0.5F));
+	}
+	
+	public static float getHorizontalPlayerBlockDistance(BlockPos blockPos)
+	{
+		float xDiff = (float)(Minecraft.getMinecraft().thePlayer.posX - blockPos.getX());
+		float zDiff = (float)(Minecraft.getMinecraft().thePlayer.posZ - blockPos.getZ());
+		return MathHelper.sqrt_float((xDiff - 0.5F) * (xDiff - 0.5F) + (zDiff - 0.5F) * (zDiff - 0.5F));
 	}
 }
