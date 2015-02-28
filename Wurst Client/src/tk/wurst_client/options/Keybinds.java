553f0a9aa42f17699c752089e33dbcc83bdb874b
Alexander01998 <contact.wurstimperium@gmail.com>
2015-02-28 15:00:18 +0100

    Add Keybinds.java

---
+++ Wurst Client/src/tk/wurst_client/options/Keybinds.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.options;
+
+import java.util.HashMap;
+
+public class Keybinds extends HashMap<String, String>
+{
+	public Keybinds()
+	{
+		put("R", ".t killaura");
+		put("H", ".t home");
+		put("LCONTROL", ".t clickgui");
+		put("B", ".t fastbreak");
+		put("F", ".t fastplace");
+		put("G", ".t flight");
+		put("U", ".t freecam");
+		put("C", ".t fullbright");
+		put("R", ".t killaura");
+		put("K", ".t multiaura");
+		put("L", ".t nuker");
+		put("J", ".t phase");
+		put("Z", ".t sneak");
+		put("GRAVE", ".t speednuker");
+	}
+}
