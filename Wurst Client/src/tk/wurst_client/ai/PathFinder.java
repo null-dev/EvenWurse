e8866b2c9262f799d98318ffec55bab9780a13f5
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-07 11:55:01 +0100

    Add PathFinder.isSafe()

--- Wurst Client/src/tk/wurst_client/ai/PathFinder.java
+++ Wurst Client/src/tk/wurst_client/ai/PathFinder.java
@@ -13,12 +13,17 @@ import java.util.PriorityQueue;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.BlockPos;
+import tk.wurst_client.Client;
+import tk.wurst_client.module.Module;
+import tk.wurst_client.module.modules.NoFall;
 
 public class PathFinder
 {
 	private BlockPos goal;
 	private PriorityQueue<PathPoint> queue;
-	private HashMap<BlockPos, PathPoint> processed = new HashMap<BlockPos, PathPoint>();
+	private HashMap<BlockPos, PathPoint> processed =
+		new HashMap<BlockPos, PathPoint>();
+	private Module noFallMod;
 	
 	public PathFinder(BlockPos goal)
 	{
@@ -37,6 +42,7 @@ public class PathFinder
 			}
 		});
 		addPoint(start, null, 0, 0);
+		noFallMod = Client.wurst.moduleManager.getModuleFromClass(NoFall.class);
 	}
 	
 	public PathPoint find()
@@ -50,22 +56,39 @@ public class PathFinder
 				break;
 			for(BlockPos neighbor : current.getNeighbors())
 			{
+				if(!isSafe(neighbor))
+					continue;
 				int newCost = current.getMovementCost() + 1;
 				// TODO: Different movement costs based on block type
-				if(!processed.containsKey(neighbor) || processed.get(neighbor).getMovementCost() > newCost)
-					addPoint(neighbor, current, newCost, newCost + getDistance(neighbor, goal));
+				if(!processed.containsKey(neighbor)
+					|| processed.get(neighbor).getMovementCost() > newCost)
+					addPoint(neighbor, current, newCost,
+						newCost + getDistance(neighbor, goal));
 			}
 		}
 		return current;
 	}
 	
-	private void addPoint(BlockPos pos, PathPoint previous, int movementCost, int priority)
+	private void addPoint(BlockPos pos, PathPoint previous, int movementCost,
+		int priority)
 	{
 		queue.add(new PathPoint(pos, previous, movementCost, priority));
 	}
 	
 	private int getDistance(BlockPos a, BlockPos b)
 	{
-		return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY()) + Math.abs(a.getZ() - b.getZ());
+		return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY())
+			+ Math.abs(a.getZ() - b.getZ());
+	}
+	
+	private boolean isSafe(BlockPos pos)
+	{
+		return !Minecraft.getMinecraft().theWorld.getBlockState(pos).getBlock()
+			.getMaterial().blocksMovement()
+			&& (Minecraft.getMinecraft().thePlayer.capabilities.isCreativeMode
+				|| noFallMod.getToggled()
+				|| Minecraft.getMinecraft().theWorld
+				.getBlockState(pos.add(0, -1, 0)).getBlock().getMaterial()
+				.blocksMovement());
 	}
 }
