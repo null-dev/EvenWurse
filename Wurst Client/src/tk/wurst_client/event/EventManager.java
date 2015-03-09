a9b50fcc55e463e26d17b1002d3913f8948289f2
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-10 00:09:01 +0100

    Create EventManager

---
+++ Wurst Client/src/tk/wurst_client/event/EventManager.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.event;
+
+public class EventManager
+{
+	public static void addListener()
+	{
+		
+	}
+
+	public static void removeListener()
+	{
+		
+	}
+	
+	public static void fireEvent()
+	{
+		
+	}
+}
