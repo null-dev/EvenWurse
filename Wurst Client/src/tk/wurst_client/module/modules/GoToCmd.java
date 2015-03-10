ebea4e3918345902fdcc2b1491fac4719a239693
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-10 20:30:24 +0100

    Add hidden mod for .goto

---
+++ Wurst Client/src/tk/wurst_client/module/modules/GoToCmd.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.module.modules;
+
+import tk.wurst_client.module.Category;
+import tk.wurst_client.module.Module;
+
+public class GoToCmd extends Module
+{
+	public GoToCmd()
+	{
+		super("GoTo", "", Category.HIDDEN);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		if(getToggled())
+			return;
+	}
+}
