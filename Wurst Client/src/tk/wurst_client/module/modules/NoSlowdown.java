5e4b8df546e2b01d7c6015a7796ed7538e318ab9
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-11 23:58:56 +0100

    Re-implemet onUpdate() in NoSlowdown

--- Wurst Client/src/tk/wurst_client/module/modules/NoSlowdown.java
+++ Wurst Client/src/tk/wurst_client/module/modules/NoSlowdown.java
@@ -9,10 +9,12 @@ package tk.wurst_client.module.modules;
 
 import net.minecraft.client.Minecraft;
 import tk.wurst_client.Client;
+import tk.wurst_client.event.EventManager;
+import tk.wurst_client.event.listeners.UpdateListener;
 import tk.wurst_client.module.Category;
 import tk.wurst_client.module.Module;
 
-public class NoSlowdown extends Module
+public class NoSlowdown extends Module implements UpdateListener
 {
 	public NoSlowdown()
 	{
@@ -23,10 +25,14 @@ public class NoSlowdown extends Module
 	}
 	
 	@Override
-	public void oldOnUpdate()
+	public void onEnable()
 	{
-		if(!getToggled())
-			return;
+		EventManager.addUpdateListener(this);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
 		if(Client.wurst.moduleManager.getModuleFromClass(YesCheat.class)
 			.getToggled())
 		{
@@ -38,5 +44,11 @@ public class NoSlowdown extends Module
 			&& Minecraft.getMinecraft().thePlayer.isInWater()
 			&& Minecraft.getMinecraft().gameSettings.keyBindJump.pressed)
 			Minecraft.getMinecraft().thePlayer.jump();
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		EventManager.removeUpdateListener(this);
 	}
 }
