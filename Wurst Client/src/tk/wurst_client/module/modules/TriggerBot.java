4b4a575f888d444e2429b77efc65c6045f1cfc24
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-14 14:14:51 +0100

    Fix TriggerBot not calculating range & speed correctly
    Resolves #179

--- Wurst Client/src/tk/wurst_client/module/modules/TriggerBot.java
+++ Wurst Client/src/tk/wurst_client/module/modules/TriggerBot.java
@@ -9,6 +9,7 @@ package tk.wurst_client.module.modules;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MovingObjectPosition.MovingObjectType;
 import tk.wurst_client.Client;
 import tk.wurst_client.event.EventManager;
 import tk.wurst_client.event.listeners.UpdateListener;
@@ -48,14 +49,23 @@ public class TriggerBot extends Module implements UpdateListener
 	public void onUpdate()
 	{
 		if(Minecraft.getMinecraft().objectMouseOver != null
-			&& Minecraft.getMinecraft().objectMouseOver.entityHit != null)
+			&& Minecraft.getMinecraft().objectMouseOver.typeOfHit == MovingObjectType.ENTITY)
 		{
 			updateMS();
-			if(hasTimePassedS(Killaura.realSpeed))
+			boolean yesCheatMode =
+				Client.wurst.moduleManager.getModuleFromClass(YesCheat.class)
+					.getToggled();
+			if(yesCheatMode && hasTimePassedS(Killaura.yesCheatSpeed)
+				|| !yesCheatMode && hasTimePassedS(Killaura.normalSpeed))
 			{
 				EntityLivingBase en =
 					(EntityLivingBase)Minecraft.getMinecraft().objectMouseOver.entityHit;
-				if(Minecraft.getMinecraft().thePlayer.getDistanceToEntity(en) <= Killaura.realRange
+				if((yesCheatMode
+					&& Minecraft.getMinecraft().thePlayer
+						.getDistanceToEntity(en) <= Killaura.yesCheatRange
+					|| !yesCheatMode
+					&& Minecraft.getMinecraft().thePlayer
+						.getDistanceToEntity(en) <= Killaura.normalRange)
 					&& EntityUtils.isCorrectEntity(en, true))
 				{
 					Criticals.doCritical();
