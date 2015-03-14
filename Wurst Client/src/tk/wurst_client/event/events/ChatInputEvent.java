ffee380e392b0df53584ad22fff1e6e245d9b68f
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-14 10:12:21 +0100

    Add ChatInputEvent

---
+++ Wurst Client/src/tk/wurst_client/event/events/ChatInputEvent.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.event.events;
+
+public class ChatInputEvent extends CancellableEvent
+{
+	private String message;
+
+	public ChatInputEvent(String message)
+	{
+		this.message = message;
+	}
+
+	public String getMessage()
+	{
+		return message;
+	}
+
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+}
