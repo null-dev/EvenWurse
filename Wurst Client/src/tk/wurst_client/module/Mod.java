9b478fc783371e9132d80fdf3dfebba669ef8270
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-16 15:06:11 +0100

    Deprecate old Mod constructor

--- Wurst Client/src/tk/wurst_client/module/Mod.java
+++ Wurst Client/src/tk/wurst_client/module/Mod.java
@@ -26,14 +26,15 @@ public class Mod
 	protected ArrayList<BasicSlider> sliders = new ArrayList<BasicSlider>();
 	private long currentMS = 0L;
 	protected long lastMS = -1L;
-	
+
+	@Deprecated
 	public Mod(String moduleName, String moduleDescription,
 		Category moduleCategory)
 	{
-		this.name = moduleName;
-		this.description = moduleDescription;
-		this.category = moduleCategory;
-		initSliders();
+		//this.name = moduleName;
+		//this.description = moduleDescription;
+		//this.category = moduleCategory;
+		//initSliders();
 	}
 	
 	@Retention(RetentionPolicy.RUNTIME)
