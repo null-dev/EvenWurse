cf237a852a879e1376c3fa8ee62fd22355f32c4c
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-16 01:01:06 +0100

    Add @ModData

---
+++ Wurst Client/src/tk/wurst_client/module/ModData.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.module;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ModData
+{	
+	String name();
+	String description();
+	Category category();
+}
