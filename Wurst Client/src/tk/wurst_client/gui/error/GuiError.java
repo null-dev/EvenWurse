94126ec250a9e2036b9f95097c5646d27ca219c5
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-18 00:32:38 +0100

    Add bug texture to error screen

--- Wurst Client/src/tk/wurst_client/gui/error/GuiError.java
+++ Wurst Client/src/tk/wurst_client/gui/error/GuiError.java
@@ -8,16 +8,21 @@
 package tk.wurst_client.gui.error;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiError extends GuiScreen
 {
+	private ResourceLocation bugTexture = new ResourceLocation("wurst/bug.png");
+
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks)
 	{
 		super.drawScreen(mouseX, mouseY, partialTicks);
 		drawDefaultBackground();
 		drawCenteredString(fontRendererObj, "§nError!§r", width / 2, height / 4, 0xffffffff);
-		drawCenteredString(fontRendererObj, "An error occurred while...", width / 2, height / 4 + 10, 0xffffffff);
+		drawCenteredString(fontRendererObj, "An error occurred while rendering Tracers.", width / 2, height / 4 + 10, 0xffffffff);
+        mc.getTextureManager().bindTexture(bugTexture);
+        drawScaledCustomSizeModalRect(width / 3 * 2, height / 3, 0, 0, 256, 256, 96, 96, 256, 256);
 		//mc.displayGuiScreen(null);
 	}
 }
