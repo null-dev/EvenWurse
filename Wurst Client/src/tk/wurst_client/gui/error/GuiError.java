6463017adeaab013695bbea61b03a2680f13996f
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-18 21:16:01 +0100

    Implement bug report button

--- Wurst Client/src/tk/wurst_client/gui/error/GuiError.java
+++ Wurst Client/src/tk/wurst_client/gui/error/GuiError.java
@@ -9,17 +9,19 @@ package tk.wurst_client.gui.error;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLEncoder;
 
 import javax.net.ssl.HttpsURLConnection;
 
-import tk.wurst_client.Client;
-import tk.wurst_client.command.Command;
-import tk.wurst_client.mod.Mod;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ResourceLocation;
+import tk.wurst_client.Client;
+import tk.wurst_client.mod.Mod;
+import tk.wurst_client.utils.MiscUtils;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -42,9 +44,12 @@ public class GuiError extends GuiScreen
 	@Override
 	public void initGui()
 	{
-		buttonList.add(new GuiButton(0, width / 2 - 154, height / 3 * 2, 100, 20, "Report Bug"));
-		buttonList.add(new GuiButton(1, width / 2 - 50, height / 3 * 2, 100, 20, "View Stacktrace"));
-		buttonList.add(new GuiButton(2, width / 2 + 54, height / 3 * 2, 100, 20, "Ignore"));
+		buttonList.add(new GuiButton(0, width / 2 - 100, height / 3 * 2, 200,
+			20, "Create/View Bug Report"));
+		buttonList.add(new GuiButton(1, width / 2 - 100, height / 3 * 2 + 24,
+			98, 20, "View Stacktrace"));
+		buttonList.add(new GuiButton(2, width / 2 + 2, height / 3 * 2 + 24, 98,
+			20, "Back to Game"));
 	}
 	
 	@Override
@@ -57,37 +62,97 @@ public class GuiError extends GuiScreen
 			case 0:
 				try
 				{
-					String url = "https://api.github.com/search/issues?q=repo:Wurst-Imperium/Wurst-Client+is:issue+";
-					url += URLEncoder.encode("test", "UTF-8");
-					HttpsURLConnection connection = (HttpsURLConnection)new URL(url).openConnection();
+					String report = "# Description\n"
+						+ "An error occurred while " + action;
+					if(listener instanceof Mod)
+						report +=
+							" "
+								+ Client.wurst.modManager.getModByClass(
+									listener.getClass()).getName();
+					report += ".\n\n"
+						+ "# Stacktrace\n";
+					StringWriter stacktraceWriter = new StringWriter();
+					e.printStackTrace(new PrintWriter(stacktraceWriter));
+					String trace = stacktraceWriter.toString();
+					report +=
+						"```\n" + trace + "```"
+							+ "\n\n# System details\n"
+							+ "- OS: " + System.getProperty("os.name") + " ("
+							+ System.getProperty("os.arch") + ")\n"
+							+ "- Java version: "
+							+ System.getProperty("java.version") + " ("
+							+ System.getProperty("java.vendor") + ")\n";
+					report = URLEncoder.encode(report, "UTF-8");
+					String query =
+						trace.replace(" ", "+").replace("\r\n", "")
+							.replace("\t", "");
+					query = query.substring(0, 128);
+					System.out.println(query);
+					String url =
+						"https://api.github.com/search/issues?q=repo:Wurst-Imperium/Wurst-Client+is:issue+"
+							+ query;
+					HttpsURLConnection connection =
+						(HttpsURLConnection)new URL(url).openConnection();
 					connection.connect();
-					JsonObject json = new JsonParser().parse(new InputStreamReader(connection.getInputStream())).getAsJsonObject();
+					JsonObject json =
+						new JsonParser().parse(
+							new InputStreamReader(connection.getInputStream()))
+							.getAsJsonObject();
 					boolean known = json.get("total_count").getAsInt() > 0;
+					if(known)
+						MiscUtils
+							.openLink("https://github.com/Wurst-Imperium/Wurst-Client/issues?q=is%3Aissue+"
+								+ query);
+					else
+					{
+						String title = "Error while " + action;
+						if(listener instanceof Mod)
+							title +=
+								" "
+									+ Client.wurst.modManager.getModByClass(
+										listener.getClass()).getName();
+						title = URLEncoder.encode(title, "UTF-8");
+						MiscUtils
+							.openLink("https://github.com/Wurst-Imperium/Wurst-Client/issues/new?title="
+								+ title + "&body=" + report);
+					}
 				}catch(Exception e)
 				{
 					e.printStackTrace();
+					Client.wurst.chat.error("Error could not be reported. :(");
+					Client.wurst.chat.message("Try reporting it manually.");
+					MiscUtils
+						.openLink("https://github.com/Wurst-Imperium/Wurst-Client/labels/bug");
+				}finally
+				{
+					actionPerformed((GuiButton)buttonList.get(2));
 				}
 				break;
 			case 1:
 				break;
 			case 2:
 				if(listener instanceof Mod)
-					Client.wurst.modManager.getModByClass(listener.getClass()).setEnabled(false);
+					Client.wurst.modManager.getModByClass(listener.getClass())
+						.setEnabled(false);
 				mc.displayGuiScreen(null);
 				break;
 			default:
 				break;
 		}
 	}
-
+	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks)
 	{
 		drawDefaultBackground();
 		mc.getTextureManager().bindTexture(bugTexture);
-		drawScaledCustomSizeModalRect(width / 4 * 3, height / 3, 0, 0, 256, 256, 96, 96, 256, 256);
-		drawCenteredString(fontRendererObj, "§nError!§r", width / 2, height / 4, 0xffffffff);
-		drawCenteredString(fontRendererObj, "An error occurred while enabling /home.", width / 2, height / 4 + 16, 0xffffffff);
-        super.drawScreen(mouseX, mouseY, partialTicks);
+		drawScaledCustomSizeModalRect(width / 4 * 3, height / 3, 0, 0, 256,
+			256, 96, 96, 256, 256);
+		drawCenteredString(fontRendererObj, "§nError!§r", width / 2,
+			height / 4, 0xffffffff);
+		drawCenteredString(fontRendererObj,
+			"An error occurred while enabling /home.", width / 2,
+			height / 4 + 16, 0xffffffff);
+		super.drawScreen(mouseX, mouseY, partialTicks);
 	}
 }
