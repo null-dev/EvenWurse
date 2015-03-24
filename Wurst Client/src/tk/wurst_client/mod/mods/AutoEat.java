00cb8e0b3881f499bc58efe85509d622403226fc
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-24 22:12:37 +0100

    Add AutoEat

---
+++ Wurst Client/src/tk/wurst_client/mod/mods/AutoEat.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.mod.mods;
+
+import tk.wurst_client.event.EventManager;
+import tk.wurst_client.event.listeners.UpdateListener;
+import tk.wurst_client.mod.Mod;
+import tk.wurst_client.mod.Mod.Category;
+import tk.wurst_client.mod.Mod.Info;
+
+@Info(category = Category.MISC,
+	description = "Automatically eats food when necessary.",
+	name = "AutoEat")
+public class AutoEat extends Mod implements UpdateListener
+{
+	@Override
+	public void onEnable()
+	{
+		EventManager.update.addListener(this);
+	}
+	
+	@Override
+	public void onUpdate()
+	{	
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		EventManager.update.removeListener(this);
+	}
+}
