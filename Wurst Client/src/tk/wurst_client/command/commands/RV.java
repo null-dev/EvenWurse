9c483eddd61d2cbee0de5ddea5e91723b3dc459f
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-25 22:27:42 +0100

    Re-implement .rv

--- Wurst Client/src/tk/wurst_client/command/commands/RV.java
+++ Wurst Client/src/tk/wurst_client/command/commands/RV.java
@@ -8,19 +8,16 @@
 package tk.wurst_client.command.commands;
 
 import tk.wurst_client.command.Command;
+import tk.wurst_client.command.Command.Info;
 import tk.wurst_client.mod.mods.RemoteView;
 
+@Info(help = "Toggles RemoteView or makes it target a specific entity.",
+	name = "rv",
+	syntax = {"[<Player>]"})
 public class RV extends Command
 {
-	public RV()
-	{
-		super("rv",
-			"Toggles RemoteView or makes it target a specific entity.",
-			"§o.rv§r [<Player>]");
-	}
-	
 	@Override
-	public void execute(String input, String[] args)
+	public void execute(String[] args)
 	{
 		if(args.length == 0)
 		{
