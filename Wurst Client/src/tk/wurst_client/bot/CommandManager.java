8d97c68e3664ac1a1d9423c38e3d08ae6bb2d7a7
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-29 16:43:15 +0200

    Add Wurst-Bot command manager

---
+++ Wurst Client/src/tk/wurst_client/bot/CommandManager.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.bot;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.TreeMap;
+
+import tk.wurst_client.bot.Command.Info;
+
+public class CommandManager
+{
+	private final TreeMap<String, Command> commands =
+		new TreeMap<String, Command>(
+			new Comparator<String>()
+			{
+				@Override
+				public int compare(String o1, String o2)
+				{
+					return o1.compareToIgnoreCase(o2);
+				}
+			});
+	
+	public CommandManager()
+	{
+		//TODO: Add commands
+	}
+	
+	public Command getCommandByClass(Class<?> commandClass)
+	{
+		return commands.get(commandClass.getAnnotation(Info.class).name());
+	}
+	
+	public Command getCommandByName(String name)
+	{
+		return commands.get(name);
+	}
+	
+	public Collection<Command> getAllCommands()
+	{
+		return commands.values();
+	}
+	
+	public int countCommands()
+	{
+		return commands.size();
+	}
+	
+	private void addCommand(Command commmand)
+	{
+		commands.put(commmand.getName(), commmand);
+	}
+}
--- Wurst Client/src/tk/wurst_client/bot/WurstBot.java
+++ Wurst Client/src/tk/wurst_client/bot/WurstBot.java
@@ -1,47 +1,95 @@
 package tk.wurst_client.bot;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import tk.wurst_client.Client;
 import net.minecraft.client.main.Main;
+import tk.wurst_client.Client;
 
 public class WurstBot implements Runnable
 {
 	private static boolean enabled = false;
+	private CommandManager commandManager;
 	
-    public static void main(String[] args)
-    {
-    	enabled = true;
-    	new Thread(new WurstBot()).start();
-        Main.main(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"});
-    }
-
+	public WurstBot()
+	{
+		commandManager = new CommandManager();
+	}
+	
+	public static void main(String[] args)
+	{
+		enabled = true;
+		new Thread(new WurstBot()).start();
+		Main.main(new String[]{"--version", "mcp", "--accessToken", "0",
+			"--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties",
+			"{}"});
+	}
+	
 	@Override
 	public void run()
 	{
 		try
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader br =
+				new BufferedReader(new InputStreamReader(System.in));
+			
 			System.out.println();
-			System.out.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
-			System.out.println("       +++#++++##++++#+#+++++++#++######+++++######+#######+++       ");
-			System.out.println("     +++++#++++##++++#+#+++++++#++#+++++##++#++++++++++#++++++++     ");
-			System.out.println("    +++++++#++#++#++#++#+++++++#++#######++++######++++#+++++++++    ");
-			System.out.println("     ++++++#++#++#++#+++#+++++#+++#+++##+++++++++++#+++#++++++++     ");
-			System.out.println("       +++++##++++##+++++#####++++#+++++##+++######++++#++++++       ");
-			System.out.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
+			System.out
+				.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
+			System.out
+				.println("       +++#++++##++++#+#+++++++#++######+++++######+#######+++       ");
+			System.out
+				.println("     +++++#++++##++++#+#+++++++#++#+++++##++#++++++++++#++++++++     ");
+			System.out
+				.println("    +++++++#++#++#++#++#+++++++#++#######++++######++++#+++++++++    ");
+			System.out
+				.println("     ++++++#++#++#++#+++#+++++#+++#+++##+++++++++++#+++#++++++++     ");
+			System.out
+				.println("       +++++##++++##+++++#####++++#+++++##+++######++++#++++++       ");
+			System.out
+				.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
 			System.out.println();
-			System.out.println("Starting WurstBot v" + Client.wurst.CLIENT_VERSION);
+			System.out.println("Starting WurstBot v"
+				+ Client.wurst.CLIENT_VERSION);
 			while(true)
 			{
-				System.out.println(br.readLine());
+				String input = br.readLine();
+				String commandName = input.split(" ")[0];
+				String[] args;
+				if(input.contains(" "))
+					args = input.substring(input.indexOf(" ") + 1).split(" ");
+				else
+					args = new String[0];
+				Command command = commandManager.getCommandByName(commandName);
+				if(command != null)
+					try
+					{
+						command.execute(args);
+					}catch(Command.SyntaxError e)
+					{
+						if(e.getMessage() != null)
+							System.err.println("Syntax error: "
+								+ e.getMessage());
+						else
+							System.err.println("Syntax error!");
+						command.printSyntax();
+					}catch(Command.Error e)
+					{
+						System.err.println(e.getMessage());
+					}catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				else
+					System.err.println("\"" + commandName
+						+ "\" is not a valid command.");
 			}
 		}catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static boolean isEnabled()
 	{
 		return enabled;
