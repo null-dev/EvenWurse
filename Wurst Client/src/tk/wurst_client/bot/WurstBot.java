315a8ac745a03fdc8ac04e4e3b152209dc49c864
Alexander01998 <contact.wurstimperium@gmail.com>
2015-03-29 23:47:24 +0200

    Add WurstBot.start()

--- Wurst Client/src/tk/wurst_client/bot/WurstBot.java
+++ Wurst Client/src/tk/wurst_client/bot/WurstBot.java
@@ -8,7 +8,7 @@ import tk.wurst_client.Client;
 import tk.wurst_client.bot.commands.Command;
 import tk.wurst_client.bot.commands.CommandManager;
 
-public class WurstBot implements Runnable
+public class WurstBot
 {
 	private static boolean enabled = false;
 	private static WurstBot wurstBot;
@@ -29,66 +29,77 @@ public class WurstBot implements Runnable
 			"{}"});
 	}
 	
-	@Override
-	public void run()
+	public void start()
 	{
-		try
+		new Thread(new Runnable()
 		{
-			BufferedReader br =
-				new BufferedReader(new InputStreamReader(System.in));
-			System.out.println();
-			System.out
-				.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
-			System.out
-				.println("       +++#++++##++++#+#+++++++#++######+++++######+#######+++       ");
-			System.out
-				.println("     +++++#++++##++++#+#+++++++#++#+++++##++#++++++++++#++++++++     ");
-			System.out
-				.println("    +++++++#++#++#++#++#+++++++#++#######++++######++++#+++++++++    ");
-			System.out
-				.println("     ++++++#++#++#++#+++#+++++#+++#+++##+++++++++++#+++#++++++++     ");
-			System.out
-				.println("       +++++##++++##+++++#####++++#+++++##+++######++++#++++++       ");
-			System.out
-				.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
-			System.out.println();
-			System.out.println("Wurst-Bot v" + Client.wurst.CLIENT_VERSION);
-			while(true)
+			@Override
+			public void run()
 			{
-				String input = br.readLine();
-				String commandName = input.split(" ")[0];
-				String[] args;
-				if(input.contains(" "))
-					args = input.substring(input.indexOf(" ") + 1).split(" ");
-				else
-					args = new String[0];
-				Command command = commandManager.getCommandByName(commandName);
-				if(command != null)
-					try
-					{
-						command.execute(args);
-					}catch(Command.SyntaxError e)
-					{
-						if(e.getMessage() != null)
-							System.err.println("Syntax error: "
-								+ e.getMessage());
-						else
-							System.err.println("Syntax error!");
-						command.printSyntax();
-					}catch(Command.Error e)
-					{
-						System.err.println(e.getMessage());
-					}catch(Exception e)
-					{
-						e.printStackTrace();
-					}
-				else
-					System.err.println("\"" + commandName
-						+ "\" is not a valid command.");
+				try
+				{
+					WurstBot.this.run();
+				}catch(Exception e)
+				{
+					e.printStackTrace();
+				}
 			}
-		}catch(Exception e)
+		}, "Wurst-Bot").start();
+	}
+	
+	private void run() throws Exception
+	{
+		BufferedReader br =
+			new BufferedReader(new InputStreamReader(System.in));
+		System.out.println();
+		System.out
+			.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
+		System.out
+			.println("       +++#++++##++++#+#+++++++#++######+++++######+#######+++       ");
+		System.out
+			.println("     +++++#++++##++++#+#+++++++#++#+++++##++#++++++++++#++++++++     ");
+		System.out
+			.println("    +++++++#++#++#++#++#+++++++#++#######++++######++++#+++++++++    ");
+		System.out
+			.println("     ++++++#++#++#++#+++#+++++#+++#+++##+++++++++++#+++#++++++++     ");
+		System.out
+			.println("       +++++##++++##+++++#####++++#+++++##+++######++++#++++++       ");
+		System.out
+			.println("           +++++++++++++++++++++++++++++++++++++++++++++++           ");
+		System.out.println();
+		System.out.println("Wurst-Bot v" + Client.wurst.CLIENT_VERSION);
+		while(true)
 		{
-			e.printStackTrace();
+			String input = br.readLine();
+			String commandName = input.split(" ")[0];
+			String[] args;
+			if(input.contains(" "))
+				args = input.substring(input.indexOf(" ") + 1).split(" ");
+			else
+				args = new String[0];
+			Command command = commandManager.getCommandByName(commandName);
+			if(command != null)
+				try
+				{
+					command.execute(args);
+				}catch(Command.SyntaxError e)
+				{
+					if(e.getMessage() != null)
+						System.err.println("Syntax error: "
+							+ e.getMessage());
+					else
+						System.err.println("Syntax error!");
+					command.printSyntax();
+				}catch(Command.Error e)
+				{
+					System.err.println(e.getMessage());
+				}catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			else
+				System.err.println("\"" + commandName
+					+ "\" is not a valid command.");
 		}
 	}
 	
@@ -96,12 +107,12 @@ public class WurstBot implements Runnable
 	{
 		return enabled;
 	}
-
+	
 	public static WurstBot getBot()
 	{
 		return wurstBot;
 	}
-
+	
 	public CommandManager getCommandManager()
 	{
 		return commandManager;
