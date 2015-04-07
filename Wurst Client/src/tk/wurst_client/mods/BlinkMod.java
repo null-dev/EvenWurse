059375d42531db4f1c53c6974d9795335330e093
Nitrogen-Dioxide <nitrogen.dioxide99@gmail.com>
2015-04-06 21:59:08 -0300

    Make Blink Manual

--- Wurst Client/src/tk/wurst_client/mods/BlinkMod.java
+++ Wurst Client/src/tk/wurst_client/mods/BlinkMod.java
@@ -10,6 +10,7 @@ package tk.wurst_client.mods;
 import java.util.ArrayList;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.network.Packet;
 import tk.wurst_client.events.EventManager;
 import tk.wurst_client.events.listeners.UpdateListener;
@@ -18,37 +19,48 @@ import tk.wurst_client.mods.Mod.Info;
 
 @Info(category = Category.MOVEMENT,
 	description = "Makes it harder for other players to see where you are.\n"
-		+ "They will think you are lagging badly, because your\n"
-		+ "position will only be updated every 3 seconds.",
+		+ "You have the control of lag in your hands!\n"
+		+ "Position will be updated everytime you disable and enable it.\n",
 	name = "Blink")
 public class BlinkMod extends Mod implements UpdateListener
 {
 	private static ArrayList<Packet> packets = new ArrayList<Packet>();
-	
+	private EntityOtherPlayerMP lagPlayer = null;
+	private double x;
+	private double y;
+	private double z;
 	@Override
 	public void onEnable()
 	{
+		x = Minecraft.getMinecraft().thePlayer.posX;
+		y = Minecraft.getMinecraft().thePlayer.posY;
+		z = Minecraft.getMinecraft().thePlayer.posZ;
+		lagPlayer =
+			new EntityOtherPlayerMP(Minecraft.getMinecraft().theWorld,
+				Minecraft.getMinecraft().thePlayer.getGameProfile());
+		lagPlayer.copyLocationAndAnglesFrom(Minecraft.getMinecraft().thePlayer);
+		lagPlayer.rotationYawHead =
+			Minecraft.getMinecraft().thePlayer.rotationYawHead;
+		Minecraft.getMinecraft().theWorld.addEntityToWorld(-69, lagPlayer);
+		
 		EventManager.update.addListener(this);
 	}
 	
 	@Override
 	public void onUpdate()
 	{
-		updateMS();
-		if(hasTimePassedM(3000))
-		{
-			for(Packet packet : packets)
-				Minecraft.getMinecraft().thePlayer.sendQueue
-					.addToSendQueue(packet);
-			packets.clear();
-			updateLastMS();
-		}
 	}
 	
 	@Override
 	public void onDisable()
 	{
+		for(Packet packet : packets)
+			Minecraft.getMinecraft().thePlayer.sendQueue
+				.addToSendQueue(packet);
+		packets.clear();
 		EventManager.update.removeListener(this);
+		Minecraft.getMinecraft().theWorld.removeEntityFromWorld(-69);
+		lagPlayer = null;
 	}
 	
 	public static void addToBlinkQueue(Packet packet)
