d68881cc8b910d9053cfdd10dd3a2e41cd3d33f2
Alexander01998 <contact.wurstimperium@gmail.com>
2015-04-21 00:19:00 +0200

    :gem: Clean up DamageCmd

--- Wurst Client/src/tk/wurst_client/commands/DamageCmd.java
+++ Wurst Client/src/tk/wurst_client/commands/DamageCmd.java
@@ -7,39 +7,46 @@
  */
 package tk.wurst_client.commands;
 
-import net.minecraft.client.*;
-import net.minecraft.network.play.client.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.C03PacketPlayer;
 
-@Cmd.Info(help = "Damages you with given amount.", name = "damage", syntax = {"[amount]"})
+@Cmd.Info(help = "Damages you with given amount.",
+	name = "damage",
+	syntax = {"[amount]"})
 public class DamageCmd extends Cmd
 {
 	@Override
 	public void execute(String[] args) throws Error
 	{
-        if (args.length == 0)
-        	syntaxError();	
-        if (!isNumeric((args[0])))
-        	syntaxError("Amount must be a number.");
+		if(args.length == 0)
+			syntaxError();
+		if(!isNumeric(args[0]))
+			syntaxError("Amount must be a number.");
 		final Minecraft mc = Minecraft.getMinecraft();
-        final double dmg = Double.parseDouble(args[0]);
-        final double x = mc.thePlayer.posX;
-        final double y = mc.thePlayer.posY;
-        final double z = mc.thePlayer.posZ;
-        final C03PacketPlayer.C04PacketPlayerPosition GroundFalse = new C03PacketPlayer.C04PacketPlayerPosition(x, y + 0.3, z, false);
-        final C03PacketPlayer.C04PacketPlayerPosition setDamage = new C03PacketPlayer.C04PacketPlayerPosition(x, y - 3.1 - dmg, z, false);
-        final C03PacketPlayer.C04PacketPlayerPosition GroundTrue = new C03PacketPlayer.C04PacketPlayerPosition(x, y, z, true);        
-        if(Minecraft.getMinecraft().isIntegratedServerRunning()
-    			&& Minecraft.getMinecraft().thePlayer.sendQueue.getPlayerInfo()
-    				.size() == 1)
-        	error("Cannot damage when in singleplayer.");
-        else if (dmg >= 0 && dmg < 40)
-        
-        {	if (!mc.thePlayer.isOnLadder() && mc.thePlayer.onGround && !mc.thePlayer.capabilities.isCreativeMode)
-        mc.getNetHandler().addToSendQueue(GroundFalse);
-        mc.getNetHandler().addToSendQueue(setDamage);
-        mc.getNetHandler().addToSendQueue(GroundTrue);
-        }
-		else
+		final double dmg = Double.parseDouble(args[0]);
+		final double x = mc.thePlayer.posX;
+		final double y = mc.thePlayer.posY;
+		final double z = mc.thePlayer.posZ;
+		final C03PacketPlayer.C04PacketPlayerPosition GroundFalse =
+			new C03PacketPlayer.C04PacketPlayerPosition(x, y + 0.3, z, false);
+		final C03PacketPlayer.C04PacketPlayerPosition setDamage =
+			new C03PacketPlayer.C04PacketPlayerPosition(x, y - 3.1 - dmg, z,
+				false);
+		final C03PacketPlayer.C04PacketPlayerPosition GroundTrue =
+			new C03PacketPlayer.C04PacketPlayerPosition(x, y, z, true);
+		if(Minecraft.getMinecraft().isIntegratedServerRunning()
+			&& Minecraft.getMinecraft().thePlayer.sendQueue.getPlayerInfo()
+				.size() == 1)
+			error("Cannot damage when in singleplayer.");
+		else if(dmg >= 0 && dmg < 40)
+		
+		{
+			if(!mc.thePlayer.isOnLadder() && mc.thePlayer.onGround
+				&& !mc.thePlayer.capabilities.isCreativeMode)
+				mc.getNetHandler().addToSendQueue(GroundFalse);
+			mc.getNetHandler().addToSendQueue(setDamage);
+			mc.getNetHandler().addToSendQueue(GroundTrue);
+		}else
 			syntaxError("Amount is too low or too high.");
 	}
 }
