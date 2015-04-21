05a1adb65c350054c6b64356d043e194fa37dd68
Alexander01998 <contact.wurstimperium@gmail.com>
2015-04-21 22:55:56 +0200

    :bug: Fix .damage not working without NoCheat+
    Resolves #540

--- Wurst Client/src/tk/wurst_client/commands/DamageCmd.java
+++ Wurst Client/src/tk/wurst_client/commands/DamageCmd.java
@@ -7,6 +7,8 @@
  */
 package tk.wurst_client.commands;
 
+import tk.wurst_client.WurstClient;
+import tk.wurst_client.mods.YesCheatMod;
 import tk.wurst_client.utils.MiscUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.C03PacketPlayer;
@@ -23,10 +25,6 @@ public class DamageCmd extends Cmd
 			syntaxError();
 		if(!MiscUtils.isInteger(args[0]))
 			syntaxError("Amount must be a number.");
-		if(Minecraft.getMinecraft().isIntegratedServerRunning()
-			&& Minecraft.getMinecraft().thePlayer.sendQueue.getPlayerInfo()
-				.size() == 1)
-			error("Cannot damage in singleplayer.");
 		if(Minecraft.getMinecraft().thePlayer.isOnLadder())
 			error("Cannot damage while climbing ladders.");
 		if(!Minecraft.getMinecraft().thePlayer.onGround)
@@ -41,22 +39,51 @@ public class DamageCmd extends Cmd
 		double x = Minecraft.getMinecraft().thePlayer.posX;
 		double y = Minecraft.getMinecraft().thePlayer.posY;
 		double z = Minecraft.getMinecraft().thePlayer.posZ;
-		Minecraft
-			.getMinecraft()
-			.getNetHandler()
-			.addToSendQueue(
-				new C03PacketPlayer.C04PacketPlayerPosition(x, y + 0.3, z,
-					false));
-		Minecraft
-			.getMinecraft()
-			.getNetHandler()
-			.addToSendQueue(
-				new C03PacketPlayer.C04PacketPlayerPosition(x, y - 3.1 - dmg,
-					z, false));
-		Minecraft
-			.getMinecraft()
-			.getNetHandler()
-			.addToSendQueue(
-				new C03PacketPlayer.C04PacketPlayerPosition(x, y, z, true));
+		if(WurstClient.INSTANCE.modManager.getModByClass(YesCheatMod.class)
+			.isEnabled())
+		{
+			Minecraft
+				.getMinecraft()
+				.getNetHandler()
+				.addToSendQueue(
+					new C03PacketPlayer.C04PacketPlayerPosition(x, y + 0.3, z,
+						false));
+			Minecraft
+				.getMinecraft()
+				.getNetHandler()
+				.addToSendQueue(
+					new C03PacketPlayer.C04PacketPlayerPosition(x, y - 3.1
+						- dmg, z, false));
+			Minecraft
+				.getMinecraft()
+				.getNetHandler()
+				.addToSendQueue(
+					new C03PacketPlayer.C04PacketPlayerPosition(x, y, z, true));
+		}else
+		{
+			for(int i = 1; i < dmg + 5; i++)
+			{
+				Minecraft
+					.getMinecraft()
+					.getNetHandler()
+					.addToSendQueue(
+						new C03PacketPlayer.C04PacketPlayerPosition(x, y + i,
+							z, false));
+			}
+			for(int i = dmg + 4; i > 0; i--)
+			{
+				Minecraft
+					.getMinecraft()
+					.getNetHandler()
+					.addToSendQueue(
+						new C03PacketPlayer.C04PacketPlayerPosition(x, y + i,
+							z, false));
+			}
+			Minecraft
+				.getMinecraft()
+				.getNetHandler()
+				.addToSendQueue(
+					new C03PacketPlayer.C04PacketPlayerPosition(x, y, z, true));
+		}
 	}
 }
