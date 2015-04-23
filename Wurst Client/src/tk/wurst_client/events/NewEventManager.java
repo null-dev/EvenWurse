815df076eadfb1ed88560dadc5e8e4667353a05e
Alexander01998 <contact.wurstimperium@gmail.com>
2015-04-23 17:05:10 +0200

    Implement processing of all event types

--- Wurst Client/src/tk/wurst_client/events/NewEventManager.java
+++ Wurst Client/src/tk/wurst_client/events/NewEventManager.java
@@ -24,17 +24,97 @@ public final class NewEventManager
 	{
 		try
 		{
-			if(type == UpdateListener.class)
+			// TODO: A more efficient way to process the type
+			if(type == ChatInputListener.class)
+				fireChatInput((ChatInputEvent)event);
+			else if(type == ChatOutputEvent.class)
+				fireChatOutput((ChatOutputEvent)event);
+			else if(type == DeathEvent.class)
+				fireDeath();
+			else if(type == GUIRenderEvent.class)
+				fireGuiRender();
+			else if(type == LeftClickEvent.class)
+				fireLeftClick();
+			else if(type == PacketInputEvent.class)
+				firePacketInput((PacketInputEvent)event);
+			else if(type == RenderEvent.class)
+				fireRender();
+			else if(type == UpdateListener.class)
 				fireUpdate();
 			else
-				throw new IllegalArgumentException("Invalid event type: " + type.getName());
+				throw new IllegalArgumentException("Invalid event type: "
+					+ type.getName());
 		}catch(Exception e)
 		{
 			handleException(e, this, "processing events", "Event type: "
 				+ event.getClass().getSimpleName());
 		}
 	}
-
+	
+	private void fireChatInput(ChatInputEvent event)
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+		{
+			if(listeners[i] == ChatInputListener.class)
+				((ChatInputListener)listeners[i + 1]).onReceivedMessage(event);
+			if(event.isCancelled())
+				break;
+		}
+	}
+	
+	private void fireChatOutput(ChatOutputEvent event)
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+		{
+			if(listeners[i] == ChatInputListener.class)
+				((ChatOutputListener)listeners[i + 1]).onSentMessage(event);
+			if(event.isCancelled())
+				break;
+		}
+	}
+	
+	private void fireDeath()
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+			if(listeners[i] == DeathListener.class)
+				((DeathListener)listeners[i + 1]).onDeath();
+	}
+	
+	private void fireGuiRender()
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+			if(listeners[i] == GUIRenderListener.class)
+				((GUIRenderListener)listeners[i + 1]).onRenderGUI();
+	}
+	
+	private void fireLeftClick()
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+			if(listeners[i] == LeftClickListener.class)
+				((LeftClickListener)listeners[i + 1]).onLeftClick();
+	}
+	
+	private void firePacketInput(PacketInputEvent event)
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+			if(listeners[i] == PacketInputListener.class)
+				((PacketInputListener)listeners[i + 1]).onReceivedPacket(event);
+	}
+	
+	private void fireRender()
+	{
+		Object[] listeners = listenerList.getListenerList();
+		for(int i = listeners.length - 2; i >= 0; i -= 2)
+			if(listeners[i] == RenderListener.class)
+				((RenderListener)listeners[i + 1]).onRender();
+	}
+	
 	private void fireUpdate()
 	{
 		Object[] listeners = listenerList.getListenerList();
