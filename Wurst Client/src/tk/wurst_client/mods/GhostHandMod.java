8e3bd867ade9f4200965f276d885eaf09b18f49b
Alexander01998 <contact.wurstimperium@gmail.com>
2015-07-07 16:07:36 +0200

    Add GhostHand mod

---
+++ Wurst Client/src/tk/wurst_client/mods/GhostHandMod.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.mods;
+
+import tk.wurst_client.WurstClient;
+
+@Mod.Info(category = Mod.Category.BLOCKS,
+	description = "Allows you to reach specific blocks through walls.\n"
+		+ "Use .ghosthand id <block id> or .ghosthand name <block name>\n"
+		+ "to specify it.",
+	name = "GhostHand")
+public class GhostHandMod extends Mod
+{
+	@Override
+	public String getRenderName()
+	{
+		return getName() + " [" + WurstClient.INSTANCE.options.ghostHandID
+			+ "]";
+	}
+}
