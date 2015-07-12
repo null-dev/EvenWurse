2856d36f641f571135e2408d52314242ad649088
Alexander01998 <contact.wurstimperium@gmail.com>
2015-07-12 07:09:40 +0200

    Add noCheatCompatible to CrashItemMod

--- Wurst Client/src/tk/wurst_client/mods/CrashItemMod.java
+++ Wurst Client/src/tk/wurst_client/mods/CrashItemMod.java
@@ -15,7 +15,8 @@ import tk.wurst_client.WurstClient;
 @Mod.Info(category = Mod.Category.EXPLOITS,
 	description = "Generates a CrashItem.\n"
 		+ "Right click a mob with it to kick nearby players from the server.",
-	name = "CrashItem")
+	name = "CrashItem",
+	noCheatCompatible = true)
 public class CrashItemMod extends Mod
 {
 	@Override
