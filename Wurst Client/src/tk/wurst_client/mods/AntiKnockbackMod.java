e97d1cae6b5be2a613e50a4a5fb842393e03b5b1
Alexander01998 <contact.wurstimperium@gmail.com>
2015-07-12 06:58:29 +0200

    Add noCheatCompatible to AntiKnockbackMod

--- Wurst Client/src/tk/wurst_client/mods/AntiKnockbackMod.java
+++ Wurst Client/src/tk/wurst_client/mods/AntiKnockbackMod.java
@@ -13,7 +13,8 @@ import tk.wurst_client.mods.Mod.Info;
 @Info(category = Category.COMBAT,
 	description = "Protects you from getting pushed by players, mobs and\n"
 		+ "fluids.",
-	name = "AntiKnockback")
+	name = "AntiKnockback",
+	noCheatCompatible = true)
 public class AntiKnockbackMod extends Mod
 {	
 	
