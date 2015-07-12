8715d87f3ba46643c91c63fe1e4d441b0ff6b7a7
Alexander01998 <contact.wurstimperium@gmail.com>
2015-07-12 07:08:13 +0200

    Add noCheatCompatible to ClickGuiMod

--- Wurst Client/src/tk/wurst_client/mods/ClickGuiMod.java
+++ Wurst Client/src/tk/wurst_client/mods/ClickGuiMod.java
@@ -16,7 +16,10 @@ import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
 
-@Info(category = Category.HIDDEN, description = "", name = "ClickGUI")
+@Info(category = Category.HIDDEN,
+	description = "",
+	name = "ClickGUI",
+	noCheatCompatible = true)
 public class ClickGuiMod extends Mod implements UpdateListener
 {
 	public ClickGuiMod()
