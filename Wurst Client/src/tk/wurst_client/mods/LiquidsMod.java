768e3b3170516204d224414536acce72088330d2
Alexander01998 <contact.wurstimperium@gmail.com>
2015-07-12 07:23:36 +0200

    Add noCheatCompatible to LiquidsMod

--- Wurst Client/src/tk/wurst_client/mods/LiquidsMod.java
+++ Wurst Client/src/tk/wurst_client/mods/LiquidsMod.java
@@ -7,37 +7,11 @@
  */
 package tk.wurst_client.mods;
 
-import tk.wurst_client.WurstClient;
-import tk.wurst_client.events.listeners.UpdateListener;
-import tk.wurst_client.mods.Mod.Category;
-import tk.wurst_client.mods.Mod.Info;
-
-@Info(category = Category.BLOCKS,
+@Mod.Info(category = Mod.Category.BLOCKS,
 	description = "Allows you to interact with liquid blocks.",
-	name = "Liquids")
-public class LiquidsMod extends Mod implements UpdateListener
+	name = "Liquids",
+	noCheatCompatible = false)
+public class LiquidsMod extends Mod
 {
-	@Override
-	public void onEnable()
-	{
-		WurstClient.INSTANCE.eventManager.add(UpdateListener.class, this);
-	}
 	
-	@Override
-	public void onUpdate()
-	{
-		if(WurstClient.INSTANCE.modManager.getModByClass(YesCheatMod.class)
-			.isEnabled())
-		{
-			noCheatMessage();
-			setEnabled(false);
-			return;
-		}
-	}
-	
-	@Override
-	public void onDisable()
-	{
-		WurstClient.INSTANCE.eventManager.remove(UpdateListener.class, this);
-	}
 }
