a08961a8eb928b10292afa2e24ebf9890c6eda3d
mcccs <can.dogan@me.com>
2015-07-31 15:38:55 +0300

    Update PhaseMod.java

--- Wurst Client/src/tk/wurst_client/mods/PhaseMod.java
+++ Wurst Client/src/tk/wurst_client/mods/PhaseMod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * Copyright Â© 2014 - 2015 | Alexander01998 | All rights reserved.
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,7 +13,7 @@ import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
 
-@Info(category = Category.MOVEMENT,
+@Info(category = Category.EXPLOITS,
 	description = "Exploits a bug in NoCheat+ that allows you to glitch\n"
 		+ "through blocks.",
 	name = "Phase")
