484b8e0d6e44e5b6b2a8e96914149c37fd58f376
Huanjohn <Huanjohnhuan@gmail.com>
2015-07-31 18:54:00 -0500

    Add "remove" Mode for PotionCmd
    The user can now remove effects with the given id from the held potion.

--- Wurst Client/src/tk/wurst_client/commands/PotionCmd.java
+++ Wurst Client/src/tk/wurst_client/commands/PotionCmd.java
@@ -11,17 +11,17 @@ import net.minecraft.potion.PotionEffect;
 import tk.wurst_client.commands.Cmd.Info;
 import tk.wurst_client.utils.MiscUtils;
 
-@Info (help="Add to/sets the efects of the held potion. Accepts arguments in groups of 3.",
+@Info (help="Add to/sets the efects of the held potion. Can also remove potion effects with the given id from held potion. "
+		+ "Accepts arguments in groups of 3 for add and set modes.",
 	name="potion",
-	syntax = {"[add] <id> <amplifier> <duration>", "[set] <id> <amplifier> <duration>"})
+	syntax = {"[add] <id> <amplifier> <duration>", "[set] <id> <amplifier> <duration>", "[remove] <id>"})
 
 public class PotionCmd extends Cmd {
 
 	@Override
 	public void execute(String[] args) throws Error {
 		
-		if(args.length == 0 || (args.length - 1) % 3 != 0 || (!args[0].equalsIgnoreCase("add") &&
-				!args[0].equalsIgnoreCase("set")))
+		if(args.length == 0)
 			syntaxError();
 		if(!Minecraft.getMinecraft().thePlayer.capabilities.isCreativeMode)
 			error("Creative mode only.");
@@ -30,6 +30,25 @@ public class PotionCmd extends Cmd {
 		if(currentItem == null || Item.getIdFromItem(currentItem.getItem()) != 373)
 			error("Please execute this command while holding a potion.");
 		NBTTagList effectList = new NBTTagList();
+		if(args.length == 2) {
+			if(!args[0].equalsIgnoreCase("Remove") || !MiscUtils.isInteger(args[1]))
+				syntaxError();
+			List oldEffects = new ItemPotion().getEffects(currentItem);
+			for(int i = 0; i < oldEffects.size(); i++) {
+				PotionEffect temp = (PotionEffect) oldEffects.get(i);
+				if(temp.getPotionID() != Integer.parseInt(args[1])) {
+					NBTTagCompound effect = new NBTTagCompound();
+					effect.setInteger("Id", temp.getPotionID());
+					effect.setInteger("Amplifier", temp.getAmplifier());
+					effect.setInteger("Duration", temp.getDuration());
+					effectList.appendTag(effect);
+				}
+			}
+			currentItem.setTagInfo("CustomPotionEffects", effectList);
+			return;
+		}
+		else if((args.length - 1) % 3 != 0)
+			syntaxError();
 		for(int i = 1; i < args.length; i++) {
 			if(!MiscUtils.isInteger(args[i])) 
 				syntaxError();
@@ -45,6 +64,8 @@ public class PotionCmd extends Cmd {
 				effectList.appendTag(effect);
 			}
 		}
+		else if(!args[0].equalsIgnoreCase("set"))
+			syntaxError();
 		for(int i = 0; i < (args.length - 1) / 3; i++) {
 			int id = Integer.parseInt(args[1+(i*3)]);
 			int amplifier = Integer.parseInt(args[2+(i*3)]);
