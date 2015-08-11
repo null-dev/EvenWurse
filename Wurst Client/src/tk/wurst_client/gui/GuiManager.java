81120b3e436557ff4d00fdf7810b022cd734fba2
Alexander01998 <contact.wurstimperium@gmail.com>
2015-08-11 10:59:56 +0200

    Use normal boilerplate for GuiManager

--- Wurst Client/src/tk/wurst_client/gui/GuiManager.java
+++ Wurst Client/src/tk/wurst_client/gui/GuiManager.java
@@ -1,10 +1,9 @@
 /*
  * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
  * 
- * Modifications made by Alexander01998 in this Source Code Form are subject
- * to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL
- * was not distributed with this file, You can obtain one at
- * http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 /*
  * Copyright (c) 2013, DarkStorm (darkstorm@evilminecraft.net)
