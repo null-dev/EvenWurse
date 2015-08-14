324029e627c5a129b7780c4a1f0705c8e6bff65e
kim-yannick <kim.jamba@gmail.com>
2015-08-14 16:38:07 +0200

    Added .give templates!

--- Wurst Client/src/tk/wurst_client/commands/GiveCmd.java
+++ Wurst Client/src/tk/wurst_client/commands/GiveCmd.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
@@ -17,43 +18,98 @@ import tk.wurst_client.utils.MiscUtils;
 
 @Info(help = "Gives you an item with custom NBT data in creative.",
 name = "give",
-syntax = {"<item name> <amount> <metadata> <NBT data>"})
+syntax = {"<item name> <amount> <metadata> <NBT data>", "template <template number> <amount>", "templates"})
 public class GiveCmd extends Cmd {
 
+	private static class ItemTemplate {
+		public Item item;
+		public String name, tag;
+		public int metadata;
+		
+		public ItemTemplate(String name, Item item, int metadata, String tag) {
+			this.name = name;
+			this.item = item;
+			this.metadata = metadata;
+			this.tag = tag;
+		}
+	}
+	
+	private ItemTemplate[] templates = new ItemTemplate[] {
+		new ItemTemplate("Knockback Stick", Items.stick, 0,
+				"{ench: [{id:19, lvl:6}], display: {Name: §6Knockback Stick}, HideFlags: 63}"),
+		new ItemTemplate("One Hit Sword", Items.diamond_sword, 0,
+				"{ench: [{id:16, lvl:32767}], display: {Name: §6One Hitter}, HideFlags: 63}")
+	};
+	
+	private int parseAmount(Item item, String amount) throws Error {
+		if (!MiscUtils.isInteger(amount))
+			syntaxError();
+		
+		int out = Integer.valueOf(amount);
+		
+		if (out < 1)
+			throw new Error("Amount has to be at least 1.");
+		
+		if (out > item.getItemStackLimit()) {
+			throw new Error("Amount is larger than the maximum stack size.");
+		}
+		
+		return out;
+	}
+	
 	@Override
 	public void execute(String[] args) throws Error {
 		EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
 		if (!player.capabilities.isCreativeMode) {
-			WurstClient.INSTANCE.chat.error("Creative mode only.");
-			return;
+			throw new Error("Creative mode only.");
 		}
+		
+		Item item = null;
+		int amount = -1;
+		int metadata = 0;
+		String nbt = null;
 		
 		if (args.length < 1)
 			syntaxError();
-	
-		ResourceLocation loc = new ResourceLocation(args[0]);
-		Item item = (Item)Item.itemRegistry.getObject(loc);
-		
-		if (item == null)
-			WurstClient.INSTANCE.chat.error("This item does not exist.");
-		else {
-			int amount = item.getItemStackLimit();
-			int metadata = 0;
+
+		if (args[0].equalsIgnoreCase("templates")) {
+			for (int i = 0; i < templates.length; i++) {
+				ItemTemplate template = templates[i];
+				WurstClient.INSTANCE.chat.message(String.format("§l§c%d§6: %s", i+1, template.name));
+			}
 			
+			return;
+		} else if (args[0].equalsIgnoreCase("template")) {
+			if (args.length < 2 || args.length > 3)
+				syntaxError();
+			
+			if (!MiscUtils.isInteger(args[1]))
+				syntaxError();
+			
+			int index = Integer.valueOf(args[1]);
+			
+			if (index < 1 || index > templates.length) {
+				WurstClient.INSTANCE.chat.error("Template index out of range.");
+				return;
+			}
+			
+			ItemTemplate template = templates[index-1];
+			item = template.item;
+			amount = item.getItemStackLimit();
+			nbt = template.tag;
+			
+			if (args.length == 3)
+				amount = parseAmount(item, args[2]);
+		} else {
+			ResourceLocation loc = new ResourceLocation(args[0]);
+			item = (Item)Item.itemRegistry.getObject(loc);
+			
+			if (item == null)
+				throw new Error("This item does not exist.");		
+			
+			amount = item.getItemStackLimit();
 			if (args.length >= 2) {
-				if (!MiscUtils.isInteger(args[1]))
-					syntaxError();
-				
-				amount = Integer.valueOf(args[1]);
-				
-				if (amount < 1) {
-					WurstClient.INSTANCE.chat.error("Amount has to be at least 1.");
-					return;
-				}
-				if (amount > item.getItemStackLimit()) {
-					WurstClient.INSTANCE.chat.error("Amount is larger than the maximum stack size.");
-					return;
-				}
+				amount = parseAmount(item, args[1]);
 			}
 			
 			if (args.length >= 3) {
@@ -63,30 +119,37 @@ public class GiveCmd extends Cmd {
 				metadata = Integer.valueOf(args[2]);
 			}
 			
-			ItemStack out = new ItemStack(item, amount, metadata);
-			
 			if (args.length >= 4) {
 				try {
-					String nbt = CommandBase.getChatComponentFromNthArg(null, args, 3).getUnformattedText();
-					out.setTagCompound(JsonToNBT.func_180713_a(nbt));
-				} catch (CommandException | NBTException e) {
-					WurstClient.INSTANCE.chat.error("An error occurred while parsing NBT data.");
+					nbt = CommandBase.getChatComponentFromNthArg(null, args, 3).getUnformattedText();
+				} catch (CommandException e) {
 					e.printStackTrace();
-					return;
+					throw new Error("An error occurred while parsing NBT data.");
 				}
 			}
-			
-			for (int i = 0; i<9; i++) {
-				if (player.inventory.getStackInSlot(i) == null) {
-					Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue(
-							new C10PacketCreativeInventoryAction(36+i, out));
-					WurstClient.INSTANCE.chat.info("Your specified item has been placed into your hotbar.");
-					return;
-				}
-			}
-			
-			WurstClient.INSTANCE.chat.error("Please clear a slot of your hotbar to use this command.");
 		}
+			
+		ItemStack out = new ItemStack(item, amount, metadata);
+		
+		if (nbt != null) {
+			try {
+				out.setTagCompound(JsonToNBT.func_180713_a(nbt));
+			} catch (NBTException e) {
+				e.printStackTrace();
+				throw new Error("An error occurred while parsing NBT data.");
+			}
+		}
+		
+		for (int i = 0; i<9; i++) {
+			if (player.inventory.getStackInSlot(i) == null) {
+				Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue(
+						new C10PacketCreativeInventoryAction(36+i, out));
+				WurstClient.INSTANCE.chat.info("Your specified item has been placed into your hotbar.");
+				return;
+			}
+		}
+		
+		WurstClient.INSTANCE.chat.error("Please clear a slot of your hotbar to use this command.");
 	}
 
 }
