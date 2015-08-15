6718e5499494d594d4a371fce3811d4cd15b1c71
Alexander01998 <contact.wurstimperium@gmail.com>
2015-08-15 21:32:11 +0200

    Add contributors to copyright notice

--- Wurst Client/src/tk/wurst_client/files/FileManager.java
+++ Wurst Client/src/tk/wurst_client/files/FileManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * Copyright © 2014 - 2015 | Alexander01998 and contributors | All rights reserved.
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -530,7 +530,7 @@ public class FileManager
 		{
 			String[] comment =
 				{
-					"Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.",
+					"Copyright © 2014 - 2015 | Alexander01998 and contributors | All rights reserved.",
 					"This Source Code Form is subject to the terms of the Mozilla Public",
 					"License, v. 2.0. If a copy of the MPL was not distributed with this",
 					"file, You can obtain one at http://mozilla.org/MPL/2.0/."};
