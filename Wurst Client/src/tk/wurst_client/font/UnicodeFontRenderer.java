9ff549763845c1aedb16a92cdc23806b92d0eaac
Alexander01998 <contact.wurstimperium@gmail.com>
2015-08-20 14:32:06 +0200

    Fix copyright notice
    Now it line-wraps correctly.

--- Wurst Client/src/tk/wurst_client/font/UnicodeFontRenderer.java
+++ Wurst Client/src/tk/wurst_client/font/UnicodeFontRenderer.java
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * Copyright © 2014 - 2015 Alexander01998 and contributors
+ * All rights reserved.
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
