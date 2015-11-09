8100ebd0e27b8f18a8350607aac342a579f7b44f
Alexander01998 <contact.wurstimperium@gmail.com>
2015-11-09 17:52:56 +0100

    Add JsonUtils
    Global GSON objects with & without pretty printing as well as a global
    JsonParser object.
    
    Creating those on the fly just feels wrong.

---
+++ Wurst Client/src/tk/wurst_client/utils/JsonUtils.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2014 - 2015 | Alexander01998 and contributors
+ * All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+
+public class JsonUtils
+{
+	public static Gson gson = new Gson();
+	public static Gson prettyGson = new GsonBuilder().setPrettyPrinting()
+		.create();
+	public static JsonParser jsonParser = new JsonParser();
+}
