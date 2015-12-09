a6b11fd8c15061bf1fb7cd95b2e4f9f478b47bc8
Alexander01998 <contact.wurstimperium@gmail.com>
2015-12-09 21:53:51 +0100

    Revert "Remove unused Event classes"
    This reverts commit 86dfa788c834d0d82dfbb5b59bbb016066636f35.

---
+++ Wurst Client/src/tk/wurst_client/events/GUIRenderEvent.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2014 - 2015 Alexander01998 and contributors
+ * All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.events;
+
+public class GUIRenderEvent extends RenderEvent
+{
+	@Override
+	public String getAction()
+	{
+		return "rendering GUI";
+	}
+}
---
+++ Wurst Client/src/tk/wurst_client/events/RenderEvent.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2014 - 2015 Alexander01998 and contributors
+ * All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.events;
+
+import net.minecraft.client.Minecraft;
+
+public class RenderEvent extends Event
+{
+	@Override
+	public String getAction()
+	{
+		return "rendering GUI";
+	}
+	
+	@Override
+	public String getComment()
+	{
+		String comment = "GUI screen: ";
+		if(Minecraft.getMinecraft().currentScreen != null)
+			comment +=
+				Minecraft.getMinecraft().currentScreen.getClass()
+					.getSimpleName();
+		else
+			comment += "null";
+		return comment;
+	}
+}
