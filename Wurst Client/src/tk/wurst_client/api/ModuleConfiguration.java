1e88aa906d7ffca3ad7e92b7394b6c21e16f775e
nulldev <contactnulldev@gmail.com>
2015-12-22 14:58:45 -0500

    More documentation for the config API.

--- Wurst Client/src/tk/wurst_client/api/ModuleConfiguration.java
+++ Wurst Client/src/tk/wurst_client/api/ModuleConfiguration.java
@@ -29,11 +29,22 @@ public class ModuleConfiguration {
 
     HashMap<String, String> config = new HashMap<>();
 
+    /**
+     * Add a string entry into the config, if a string with this key already exists, it will be removed.
+     * @param key The key of the entry to add.
+     * @param value The value of the entry.
+     */
     public void putString(String key, String value) {
         if(config.containsKey(key)) config.remove(key);
         config.put(key, value);
     }
 
+    /**
+     * Get a string entry from the config, the default value will be returned if it does not exist.
+     * @param key The key of the entry
+     * @param defaultValue The default value if the entry does not exist.
+     * @return The value of the entry.
+     */
     public String getString(String key, String defaultValue) {
         if(!config.containsKey(key)){
             config.put(key, defaultValue);
@@ -47,6 +58,10 @@ public class ModuleConfiguration {
                 + "</ITALIC>' of type <UNDERLINE>" + type + "</UNDERLINE> but value was of different type! Please confirm the value is an <UNDERLINE>" + type + "</UNDERLINE>!"));
     }
 
+    /**
+     * Similar to {@link ModuleConfiguration#getString(String, String)} except parses the value of the entry to an int.
+     * It also warns the user if value is the wrong type.
+     */
     public int getInt(String key, int defaultValue) {
         String s = String.valueOf(defaultValue);
         String ret = getString(key, s);
@@ -61,6 +76,10 @@ public class ModuleConfiguration {
         return defaultValue;
     }
 
+    /**
+     * Similar to {@link ModuleConfiguration#getString(String, String)} except parses the value of the entry to a float.
+     * It also warns the user if value is the wrong type.
+     */
     public float getFloat(String key, float defaultValue) {
         String s = String.valueOf(defaultValue);
         String ret = getString(key, s);
@@ -75,6 +94,10 @@ public class ModuleConfiguration {
         return defaultValue;
     }
 
+    /**
+     * Similar to {@link ModuleConfiguration#getString(String, String)} except parses the value of the entry to a boolean.
+     * It also warns the user if value is the wrong type.
+     */
     public boolean getBoolean(String key, boolean defaultValue) {
         String s = String.valueOf(defaultValue);
         String ret = getString(key, s);
@@ -89,19 +112,32 @@ public class ModuleConfiguration {
         return defaultValue;
     }
 
+    /**
+     * Check if the module configuration contains an entry with a specified key
+     * @param key The key of the entry
+     * @return Whether or not an entry with this key exists.
+     */
     public boolean contains(String key) {
         return config.containsKey(key);
     }
 
+    /**
+     * Get the config HashMap
+     * @return The internal config HashMap
+     */
     public HashMap<String, String> getConfig() {
         return config;
     }
 
+    /**
+     * Get the name of the module owning the configuration
+     * @return The name of the module owning the configuration
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
