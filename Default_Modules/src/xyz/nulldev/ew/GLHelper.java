cecc2a134c849785650de78f2da166ebbebfae08
nulldev <contactnulldev@gmail.com>
2015-12-24 13:14:30 -0500

    Fix waypoint tag rendering bugging out original Minecraft 3d models.

--- Default_Modules/src/xyz/nulldev/ew/GLHelper.java
+++ Default_Modules/src/xyz/nulldev/ew/GLHelper.java
@@ -186,7 +186,6 @@ public class GLHelper {
         GL11.glDisable( GL11.GL_BLEND );
         GL11.glEnable( GL11.GL_DEPTH_TEST );
         GL11.glEnable( GL11.GL_LIGHTING );
-        GlStateManager.disableAlpha();
         GlStateManager.color(1, 1, 1, 1);
         GL11.glColor4f( 1.0F, 1.0F, 1.0F, 1.0F );
         GL11.glPopMatrix( );
