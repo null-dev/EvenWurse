df451af67d25a5c435c03ead42125f6c14fb5dc8
nulldev <contactnulldev@gmail.com>
2015-12-29 21:05:39 -0500

    Increment version number to 1.37.

--- Wurst Client/src/tk/wurst_client/WurstClient.java
+++ Wurst Client/src/tk/wurst_client/WurstClient.java
@@ -29,8 +29,8 @@ public class WurstClient
 	public static WurstClient INSTANCE = new WurstClient();
 	
 	public static final String VERSION = "2.12";
-	public static final int EW_VERSION_CODE = 136;
-	public static final String EW_VERSION = "1.36 (Alpha)";
+	public static final int EW_VERSION_CODE = 137;
+	public static final String EW_VERSION = "1.37 (Beta)";
 	public boolean startupMessageDisabled = false;
 	
 	public ChatManager chat;
