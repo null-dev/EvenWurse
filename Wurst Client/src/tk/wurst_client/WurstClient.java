3f2b632aa137d29fd313b9fda45df44feccc5453
nulldev <contactnulldev@gmail.com>
2016-01-10 13:11:31 -0500

    (UPSTREAM) Change base Wurst version to 2.13pre2

--- Wurst Client/src/tk/wurst_client/WurstClient.java
+++ Wurst Client/src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public class WurstClient
 {
 	public static WurstClient INSTANCE = new WurstClient();
 	
-	public static final String VERSION = "2.13pre1";
+	public static final String VERSION = "2.13pre2";
 	public static final int EW_VERSION_CODE = 141;
 	public static final String EW_VERSION = "1.41 (Beta)";
 	public static final String BASE_VERSION = "1.8";
