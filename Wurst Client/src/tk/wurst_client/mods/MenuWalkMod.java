5e1afe888ac16b69d5362de63f1566a1ddce5b6f
nulldev <contactnulldev@gmail.com>
2016-01-10 11:42:54 -0500

    (UPSTREAM) Fix MenuWalk breaking Navigator

--- Wurst Client/src/tk/wurst_client/mods/MenuWalkMod.java
+++ Wurst Client/src/tk/wurst_client/mods/MenuWalkMod.java
@@ -11,31 +11,31 @@ package tk.wurst_client.mods;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.GuiIngameMenu;
-
 import org.lwjgl.input.Keyboard;
+import tk.wurst_client.navigator.gui.NavigatorScreen;
 
 @Mod.Info(category = Mod.Category.MOVEMENT,
-	description = "Allows you to walk while viewing a menu (e.g. the inventory\n"
-		+ "menu).",
-	name = "MenuWalk")
-public class MenuWalkMod extends Mod
-{
-	public boolean shouldAllowWalking()
-	{
-		// check if mod is active
-		if(!isActive())
-			return false;
-		
-		// check if there is a player to move
-		Minecraft mc = Minecraft.getMinecraft();
-		if(mc.thePlayer == null)
-			return false;
-		
-		// check if player is viewing chat
-		if((mc.currentScreen instanceof GuiChat) || (mc.currentScreen instanceof GuiIngameMenu))
-			return false;
-		
-		// check if inventory key is pressed
-		return !Keyboard.isKeyDown(mc.gameSettings.keyBindInventory.getKeyCode());
-	}
+        description = "Allows you to walk while viewing a menu (e.g. the inventory\n"
+                + "menu).",
+        name = "MenuWalk")
+public class MenuWalkMod extends Mod {
+    public boolean shouldAllowWalking() {
+        // check if mod is active
+        if(!isActive())
+            return false;
+
+        // check if there is a player to move
+        Minecraft mc = Minecraft.getMinecraft();
+        if(mc.thePlayer == null)
+            return false;
+
+        // check if player is viewing chat
+        if((mc.currentScreen instanceof GuiChat)
+                || (mc.currentScreen instanceof GuiIngameMenu)
+                || (mc.currentScreen instanceof NavigatorScreen))
+            return false;
+
+        // check if inventory key is pressed
+        return !Keyboard.isKeyDown(mc.gameSettings.keyBindInventory.getKeyCode());
+    }
 }
