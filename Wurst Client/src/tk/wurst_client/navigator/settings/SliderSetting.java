ff9c32dc6ff543d73330ce1dfebddbc06e1bff99
nulldev <contactnulldev@gmail.com>
2016-01-10 12:17:17 -0500

    (UPSTREAM) Create SliderSetting class

---
+++ Wurst Client/src/tk/wurst_client/navigator/settings/SliderSetting.java
@@ -0,0 +1,10 @@
+package tk.wurst_client.navigator.settings;
+
+import org.darkstorm.minecraft.gui.component.basic.BasicSlider;
+
+/**
+ * Project: EvenWurse
+ * Created: 10/01/16
+ * Author: nulldev
+ */
+public class SliderSetting extends BasicSlider implements NavigatorSetting {}
