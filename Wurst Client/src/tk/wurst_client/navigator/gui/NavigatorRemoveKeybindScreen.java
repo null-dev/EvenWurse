464b2275ad19bab3c3147a48bc0d8368b2c9049d
nulldev <contactnulldev@gmail.com>
2016-01-10 11:40:42 -0500

    (UPSTREAM) Use util methods in NavigatorRemoveKeybindScreen

--- Wurst Client/src/tk/wurst_client/navigator/gui/NavigatorRemoveKeybindScreen.java
+++ Wurst Client/src/tk/wurst_client/navigator/gui/NavigatorRemoveKeybindScreen.java
@@ -140,15 +140,7 @@ public class NavigatorRemoveKeybindScreen extends NavigatorScreen {
                 glColor4f(0.25F, 0.25F, 0.25F, 0.25F);
 
             // button
-            glBegin(GL_QUADS);
-            {
-                glVertex2i(x1, y1);
-                glVertex2i(x2, y1);
-                glVertex2i(x2, y2);
-                glVertex2i(x1, y2);
-            }
-            glEnd();
-            RenderUtil.boxShadow(x1, y1, x2, y2);
+            drawBox(x1, y1, x2, y2);
 
             // text
             drawString(Fonts.segoe15, key + ": " + keybind.getDescription()
@@ -183,15 +175,7 @@ public class NavigatorRemoveKeybindScreen extends NavigatorScreen {
 
             // button
             glDisable(GL_TEXTURE_2D);
-            glBegin(GL_QUADS);
-            {
-                glVertex2i(x1, y1);
-                glVertex2i(x2, y1);
-                glVertex2i(x2, y2);
-                glVertex2i(x1, y2);
-            }
-            glEnd();
-            RenderUtil.boxShadow(x1, y1, x2, y2);
+            drawBox(x1, y1, x2, y2);
 
             // text
             drawCenteredString(Fonts.segoe18, button.displayString,
