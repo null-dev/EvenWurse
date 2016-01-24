4441f7a17ac15beab69e37a5b60ca1136c005c47
nulldev <contactnulldev@gmail.com>
2016-01-24 16:30:32 -0500

    (UPSTREAM) Use getters & setters for Target settings

--- Wurst Client/src/tk/wurst_client/special/TargetFeature.java
+++ Wurst Client/src/tk/wurst_client/special/TargetFeature.java
@@ -17,16 +17,17 @@ import java.util.ArrayList;
 @SpecialFeature.Info(description = "Controls what entities are targeted by " + "other features (e.g. Killaura).",
         name = "Target")
 public class TargetFeature extends SpecialFeature {
-    public boolean players = true;
-    public boolean animals = true;
-    public boolean monsters = true;
-    public boolean golems = true;
+    private boolean players = true;
+    private boolean animals = true;
+    private boolean monsters = true;
+    private boolean golems = true;
 
-    public boolean sleeping_players = false;
-    public boolean invisible_players = false;
-    public boolean invisible_mobs = false;
+    //Please note that these variables have been renamed to follow the official Java codestyle
+    private boolean sleepingPlayers = false;
+    private boolean invisiblePlayers = false;
+    private boolean invisibleMobs = false;
 
-    public boolean teams = false;
+    private boolean teams = false;
 
     @Override
     public String getPrimaryAction() {
@@ -42,11 +43,11 @@ public class TargetFeature extends SpecialFeature {
     public ArrayList<NavigatorSetting> getSettings() {
         ArrayList<NavigatorSetting> settings = super.getSettings();
 
-        //FIXME Needs to switch to API
-        // STOPSHIP: 24/01/16 NEEDS TO CALL API
+        //FIXME Migrate to configuration API
         for (Field field : TargetFeature.class.getFields()) {
             if (!field.getType().equals(boolean.class)) continue;
 
+            //FIXME Fix prettyfication to be compatible with Java code style (or just migrate this to the config api again)
             String name =
                     field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1).replace("_", " ");
 
@@ -70,5 +71,69 @@ public class TargetFeature extends SpecialFeature {
         }
 
         return settings;
+    }
+
+    public boolean isPlayers() {
+        return players;
+    }
+
+    public void setPlayers(boolean players) {
+        this.players = players;
+    }
+
+    public boolean isAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(boolean animals) {
+        this.animals = animals;
+    }
+
+    public boolean isMonsters() {
+        return monsters;
+    }
+
+    public void setMonsters(boolean monsters) {
+        this.monsters = monsters;
+    }
+
+    public boolean isGolems() {
+        return golems;
+    }
+
+    public void setGolems(boolean golems) {
+        this.golems = golems;
+    }
+
+    public boolean isSleepingPlayers() {
+        return sleepingPlayers;
+    }
+
+    public void setSleepingPlayers(boolean sleepingPlayers) {
+        this.sleepingPlayers = sleepingPlayers;
+    }
+
+    public boolean isInvisiblePlayers() {
+        return invisiblePlayers;
+    }
+
+    public void setInvisiblePlayers(boolean invisiblePlayers) {
+        this.invisiblePlayers = invisiblePlayers;
+    }
+
+    public boolean isInvisibleMobs() {
+        return invisibleMobs;
+    }
+
+    public void setInvisibleMobs(boolean invisibleMobs) {
+        this.invisibleMobs = invisibleMobs;
+    }
+
+    public boolean isTeams() {
+        return teams;
+    }
+
+    public void setTeams(boolean teams) {
+        this.teams = teams;
     }
 }
